e2f4d3f708595845c94458ea07599c72336d3ef8
yochp <peled770@gmail.com>
2018-08-15 18:26:37 +0300

    add name to the task c

---
+++ src/app/task-tag/task-tag.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-tag',
+  templateUrl: './task-tag.component.html',
+  styleUrls: ['./task-tag.component.css']
+})
+export class TaskTagComponent implements OnInit {
+
+  constructor() { }
+
+
+    tags  = [
+     {id: 1, name: 'task1'},
+    {id: 2, name: 'task2'},
+    {id: 3, name: 'task3'},
+     ];
+
+
+
+
+  ngOnInit() {
+  }
+
+}
