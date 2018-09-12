309353807a1fd693119b512bf269eeb66182cf5b
yochp <peled770@gmail.com>
2018-09-12 20:51:39 +0300

    add a new task

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -1,9 +1,29 @@
+// export class Task  {
+//   id: Number;
+//   name?: String;
+//   owner: String;
+//   admin: String;
+//   description?: String;
+//   notes: [ { content: String, author: String } ];
+//   stageId: Number;
+// }
+//
+
+
 export class Task  {
-  id: Number;
-  name: String;
-  owner: String;
-  admin: String;
-  notes: [ { content: String, author: String } ];
-  stageId: Number;
+  id: number;
+  name: string;
+  owner: string;
+  admin: string;
+  description: string;
+  notes: [ { content: string, author: string } ];
+  stageId: number;
+  timeStamp: any;
+  code: number;
+
+  constructor() {
+    this.timeStamp = Date.now();
+    this.timeStamp = new Date(this.timeStamp ).toISOString();
+  }
 }
 
--- src/app/task-serv.service.ts
+++ src/app/task-serv.service.ts
@@ -1,30 +1,30 @@
 import {Injectable} from '@angular/core';
 import {Task} from './models/task.model';
 // import {TASK} from './mocks/task.mocks';
-import { Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskServService {
-
   private taskUrl = 'api/task';  // URL to web api
 
-  constructor(  private http: HttpClient) {
+  constructor(private http: HttpClient) {
   }
 
 
   geTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.taskUrl);
-    // of(TASK);
   }
 
-//
-//   createNewTask(task) {
-// TASK.push(task);
-// return TASK;
-//   }
+
+
+  createNewTaskFuncServ(newTask): Observable<Task[]> {
+// TODO insert/push the new task to database
+    return this.http.post<Task[]>(this.taskUrl, newTask);
+
+  }
 
 }
